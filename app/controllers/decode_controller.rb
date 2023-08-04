5abb32c5965b5f3b8886c51df10a91fb5d00a681
torcuata22 <marilynjmarquez@gmail.com>
2023-08-04 17:17:08 -0400

    encoding is working, decoding is not

--- app/controllers/decode_controller.rb
+++ app/controllers/decode_controller.rb
@@ -1,4 +1,30 @@
 class DecodeController < ApplicationController
   def index
-  end
-end
+    alphabet = ('a'..'z').to_a 
+    num = params[:num].to_i
+    phrase = params[:phrase]&.downcase || ""
+
+    puts "Receiver params[:phrase]: #{params[:phrase]}"
+    puts "Receiver params[:num]: #{params[:num]}"
+
+    words = phrase.split(' ')
+    decoded_phrase = ""
+    words.each_with_index do |word, index| 
+      decoded_word = "" 
+      word.each_char do |char|
+          if alphabet.include?(char)
+              i = alphabet.index(char)
+              j = (alphabet.length + i - num) % alphabet.length 
+              letter = alphabet[j] 
+              decoded_word << letter
+          else
+              decoded_word << char
+          end
+      end
+      decoded_phrase << decoded_word
+      decoded_phrase << ' '
+    end
+    
+    @decoded_phrase = decoded_phrase
+  end  
+ end
--- app/controllers/encode_controller.rb
+++ app/controllers/encode_controller.rb
@@ -1,4 +1,31 @@
 class EncodeController < ApplicationController
   def index
+    alphabet = ('a'..'z').to_a 
+    num = params[:num].to_i
+    phrase = params[:phrase]&.downcase || ""
+
+    words = phrase.split(' ')
+    encoded_phrase = ""
+
+    words.each_with_index do |word, index|
+        encoded_word = ""
+        word.each_char do |char|
+          if alphabet.include?(char)
+              i = alphabet.index(char)
+              j = (alphabet.length + i + num) % alphabet.length 
+              letter = alphabet[j]
+              encoded_word << letter
+          else
+              encoded_word << char
+          end
+        end
+        encoded_phrase << encoded_word
+        encoded_phrase << ' '
+    end
+
+   @encoded_phrase = encoded_phrase
+
   end
 end
+  
+
